9258ac4373a97efe8ff3cb6538ed793e67ba1c1a
mnkhan78 <140829021+mnkhan78@users.noreply.github.com>
2025-04-15 04:58:31 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const names = "Mohammed"
+let age = 23
+var place = "kolkata"
+email = "nas@mnk.com"
+
+//changing of value of const is not allowed
+
+/* 
+prefer not to use var 
+because of issue in block scoope and functional scope
+*/
+
+console.table([names, age, place, email])
