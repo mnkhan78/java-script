eded85c9f0e463239b1491f5d532944f61c25bea
mnkhan78 <140829021+mnkhan78@users.noreply.github.com>
2025-04-16 03:47:31 +0000

    date and time

---
+++ 01_basics/07_dateTime.js
@@ -0,0 +1,21 @@
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+
+// console.log(typeof myDate);
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+
+let newDate = new Date()
+// console.log(newDate);
+let istDate = newDate.toLocaleString("en-IN", { timeZone: "Asia/Kolkata" });
+console.log(istDate);
+
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+
